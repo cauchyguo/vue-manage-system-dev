35c2e6482484b0d89c3e080cb76d88a355271f3a
cauchy <cauchyguo@gmail.com>
2023-03-15 22:39:45 +0800

    初版，实现登录模块

--- src/main.js
+++ src/main.js
@@ -25,9 +25,11 @@ axios.defaults.baseURL = 'http://127.0.0.1:5000'
 
 //使用钩子函数对路由进行权限跳转
 router.beforeEach((to, from, next) => {
-    document.title = `${to.meta.title} | vue-manage-system`;
+    document.title = `${to.meta.title} | vue-web-manage-system`;
     const role = localStorage.getItem('ms_username');
+    // 限制权限
     if (!role && to.path !== '/login') {
+        // 如果role为空且访问其他路由，强制转入login页面
         next('/login');
     } else if (to.meta.permission) {
         // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
